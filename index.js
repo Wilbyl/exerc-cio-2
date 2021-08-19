9f4ed6159c0cc31cff8fb45fe597d005323b5b00
Wilbyl <85300573+Wilbyl@users.noreply.github.com>
2021-08-19 02:05:42 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+
+
+const nome = "wil" ;
+ console.log(nome);
+
+let idade = "25";
+ console.log(idade);
+
+let r1 = "vamos nessa";
+ let r2 = "meu camarada";
+   let junto = r1+r2;
+    console.log = (junto);
+
+//exercicio de imprimir e fundir na tela/
+
+let mensagem = "Olá, Bluemersssss";
+console.log(`O tamanho da String mensagem é ${mensagem.length} letras`)
+
+//usar o $ so quando tiver no meio de uma frrase ,e usar ".length" para trazer a quantidade de letras
